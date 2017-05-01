b4ab3aa5451a8cf4ec7f442b2e533ad63f810b4e
Tom Kouwenhoven <tomkouwenhoven@hotmail.com>
2017-05-01 12:33:44 +0200

    Some python code added, and literature study done

--- Code/Python/readCSV_V2.py
+++ Code/Python/readCSV_V2.py
@@ -9,15 +9,6 @@ standardSteps = 650
 standardFood = 1
 standardWork = 1
 
-thresDrinks = 0.8
-thresPeople = 0.8
-thresLight = 0.8
-thresTemp = 0.8
-thresHum = 0.8
-thresSteps = 0.8
-thresFood = 0.8
-thresWork = 0.8
-
 weightDrinks = 0.5
 weightPeople = 0.2
 weightLight = 0.3
@@ -28,7 +19,6 @@ weightFood = 0.4
 weightWork = 0.6
 
 standardString = [standardDrinks, standardSteps, standardFood, standardLight, standardTemp, standardHum, standardPeople, standardWork]
-threshString = [thresDrinks, thresSteps, thresFood, thresLight, thresTemp, thresHum, thresPeople,  thresWork]
 weightString = [weightDrinks, weightSteps, weightFood, weightLight, weightTemp, weightHum, weightPeople, weightWork]
 
 def read_csv():
@@ -38,47 +28,33 @@ def read_csv():
 def calculatePercentages(relevantData):
 	percentageList=[]
 	for i, standard in zip(relevantData, standardString):
-		percentageList.append(i/standard)
+		if i < standard:
+			percentageList.append(i/standard)
+		else:
+			percentageList.append(1)
 	return(percentageList)
-
 
 def calculateTotal(hour):
 	conceptList = []
 	totalPhysicalScore = 0
-	for concept, thresh, weight in zip(hour[0:3], threshString[0:3], weightString[0:3]):
-		#print(concept, thresh, weight)
-		if (concept >= thresh):
-			totalPhysicalScore += (1 * weight)
-			#print("YES")
-		else:
-			#print("NO")
-			totalPhysicalScore += (concept * weight)
+	for concept, standard, weight in zip(hour[0:3], standardString[0:3], weightString[0:3]):
+		#print(concept, standard, weight)
+		totalPhysicalScore += (concept * weight)
 	conceptList.append(totalPhysicalScore)
 
 	totalEnvironmentalScore = 0
-	for concept, thresh, weight in zip(hour[3:6], threshString[3:6], weightString[3:6]):
-		#print(concept, thresh, weight)
-		if (concept >= thresh):
-			totalEnvironmentalScore += (1 * weight)
-			#print("YES")
-		else:
-			#print("NO")
-			totalEnvironmentalScore += (concept * weight)
+	for concept, standard, weight in zip(hour[3:6], standardString[3:6], weightString[3:6]):
+		#print(concept, standard, weight)
+		totalEnvironmentalScore += (concept * weight)
 	conceptList.append(totalEnvironmentalScore)
 
 	totalMentalScore = 0
-	for concept, thresh, weight in zip(hour[6:8], threshString[6:8], weightString[6:8]):
-		#print(concept, thresh, weight)
-		if (concept >= thresh):
-			totalMentalScore += (1 * weight)
-			#print("YES")
-		else:
-			#print("NO")
-			totalMentalScore += (concept * weight)
+	for concept, standard, weight in zip(hour[6:8], standardString[6:8], weightString[6:8]):
+		#print(concept, standard, weight)
+		totalMentalScore += (concept * weight)
 	conceptList.append(totalPhysicalScore)
 
 	return conceptList
-
 
 data =read_csv()
 
@@ -88,8 +64,6 @@ for index, row in data.iterrows():
 	relevantData = list(map(float,singleRow[0:8]))
 	percentages = calculatePercentages(relevantData)
 	calculatedPercentagesList.append(percentages)
-
-
 	
 for hour in calculatedPercentagesList:
 	#print(hour)
@@ -97,6 +71,7 @@ for hour in calculatedPercentagesList:
 	
 	print(a)
 	
+
 
 
 
