e1b23c52d2039fc239f0e6c77a58ecb342d2d9a1
jvanrijswijck <j.p.van.rijswijck@student.vu.nl>
2017-05-02 12:16:39 +0200

    added image and gif open code

---
+++ Code/Python/test.py
@@ -0,0 +1,21 @@
+# load and show an animated gif file using module pyglet
+# download module pyglet from: http://www.pyglet.org/download.html
+# the animated dinosaur-07.gif file is in the public domain
+# download from http://www.gifanimations.com
+# tested with Python2.5 and pyglet1.1a2  by  vegaseat   22apr2008
+import pyglet
+# pick an animated gif file you have in the working directory
+ag_file = "test3.gif"
+animation = pyglet.resource.animation(ag_file)
+sprite = pyglet.sprite.Sprite(animation)
+# create a window and set it to the image size
+win = pyglet.window.Window(width=sprite.width, height=sprite.height)
+# set window background color = r, g, b, alpha
+# each value goes from 0.0 to 1.0
+green = 0, 1, 0, 1
+pyglet.gl.glClearColor(*green)
+@win.event
+def on_draw():
+    win.clear()
+    sprite.draw()
+pyglet.app.run()
